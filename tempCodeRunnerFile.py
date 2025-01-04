8ed9125dfab317d15ebc7d7336335e39fe15eb11
Soham-Debnath <20debnathsoham04@gmail.com>
2025-01-04 20:38:52 +0530

    tempcode

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+while True:
+
+    #     #  Listen for the wake word "Jarvis"
+    #     # obtain audio from the microphone
+    #     r = sr.Recognizer()
+    #     with sr.Microphone() as source:
+    #         print("Listening...")
+    #         audio = r.listen(source, timeout=2)
+
+    #     print("recognizing...")
+    #     try:
+    #         command = r.recognize_google(audio)
+    #         print(command)
+    #     except Exception as e:
+    #        print("Error:", e)
