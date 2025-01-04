f463ffc2fb5fcfde980bb066d792b9f2dd1776cc
Soham-Debnath <20debnathsoham04@gmail.com>
2025-01-04 20:38:00 +0530

    music_library

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "kiss":"https://www.youtube.com/watch?v=8cVkLeCqUHk",
+    "three":"https://www.youtube.com/watch?v=yom3HewJev4",
+    "smile":"https://www.youtube.com/watch?v=kPa7bsKwL-c",
+    "tum hi ho":"https://www.youtube.com/watch?v=Umqb9KENgmk",
+    "nain":"https://www.youtube.com/watch?v=q8TTdhoxWm8",
+    "myfavouritesong":"https://www.youtube.com/watch?v=dx4Teh-nv3A"
+}
